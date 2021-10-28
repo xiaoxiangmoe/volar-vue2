4d5d1625f58dde6878292d0e3aaa5b34d94bc949
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2021-10-28 14:15:45 +0800

    Bug for unplugin-vue2-script-setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from '@vue/composition-api';
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button type="button" @click="count++">count is: {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import { createVuePlugin } from 'vite-plugin-vue2'
+import ScriptSetup from 'unplugin-vue2-script-setup/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    createVuePlugin(/* options */),
+    ScriptSetup({ /* options */ }),
+
+  ]
+})
